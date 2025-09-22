abcd9dde34d9cb3dbf2a84b9c50d4e3ada8f0332
reocat <ng.ct_ml@tuta.io>
2025-09-23 00:44:28 +0300

    magic4

--- drivers/bus/mhi/controllers/mhi_arch_qcom.c
+++ drivers/bus/mhi/controllers/mhi_arch_qcom.c
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.*/
 
+#ifdef CONFIG_ARM
 #include <asm/dma-iommu.h>
+#else
+#include <linux/dma-iommu.h>
+#endif
 #include <linux/async.h>
 #include <linux/debugfs.h>
 #include <linux/device.h>
