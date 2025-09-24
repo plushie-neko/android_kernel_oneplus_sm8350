08c16633786930b869f6356620bf71ebf3df0bfc
reocat <ng.ct_ml@tuta.io>
2025-09-24 18:28:13 +0300

    magic38

--- techpack/audio/asoc/codecs/wsa883x/wsa883x.h
+++ techpack/audio/asoc/codecs/wsa883x/wsa883x.h
@@ -14,30 +14,30 @@
 
 #if IS_ENABLED(CONFIG_SND_SOC_WSA883X)
 int wsa883x_set_channel_map(struct snd_soc_component *component,
-				   u8 *port, u8 num_port, unsigned int *ch_mask,
-				   unsigned int *ch_rate, u8 *port_type);
+		   u8 *port, u8 num_port, unsigned int *ch_mask,
+		   unsigned int *ch_rate, u8 *port_type);
 
 
 int wsa883x_codec_info_create_codec_entry(
-					struct snd_info_entry *codec_root,
-					struct snd_soc_component *component);
+				struct snd_info_entry *codec_root,
+				struct snd_soc_component *component);
 int wsa883x_codec_get_dev_num(struct snd_soc_component *component);
 #else
 static int wsa883x_set_channel_map(struct snd_soc_component *component,
-				   u8 *port, u8 num_port, unsigned int *ch_mask,
-				   unsigned int *ch_rate, u8 *port_type)
+		   u8 *port, u8 num_port, unsigned int *ch_mask,
+		   unsigned int *ch_rate, u8 *port_type)
 {
 	return 0;
 }
 
 static int wsa883x_codec_info_create_codec_entry(
-					struct snd_info_entry *codec_root,
-					struct snd_soc_component *component)
+				struct snd_info_entry *codec_root,
+				struct snd_soc_component *component)
 {
 	return 0;
 }
 
-static int wsa883x_codec_get_dev_num(struct snd_soc_component *component)
+static int __maybe_unused wsa883x_codec_get_dev_num(struct snd_soc_component *component)
 {
 	return 0;
 }
