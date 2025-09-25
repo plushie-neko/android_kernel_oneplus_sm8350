73c42046665f55e579d4a846c80d0392a8a008f2
reocat <ng.ct_ml@tuta.io>
2025-09-25 10:01:49 +0300

    magic40

--- drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_proc.c
+++ drivers/input/touchscreen/oplus_touchscreen_v2/touchpanel_proc.c
@@ -3050,214 +3050,11 @@ static int init_debug_info_proc(struct touchpanel_data *ts)
  * we need to set touchpanel_data struct as private_data to those file_inode
  * Returning zero(success) or negative errno(failed)
  */
-/* Define the proc nodes as global to avoid stack overflow */
-static tp_proc_node tp_proc_nodes_template[] = {
-	{
-		"oplus_optimized_time", 0666, NULL, &proc_optimized_time_fops, NULL, false,
-		false
-	},
-	{"tp_index", 0666, NULL, &proc_tp_index_ops, NULL, false, false},
-	{"debug_level", 0644, NULL, &proc_debug_level_ops, NULL, false, true},
-	{
-		"double_tap_enable", 0666, NULL, &proc_gesture_control_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"coordinate", 0444, NULL, &proc_coordinate_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"game_switch_enable", 0666, NULL, &proc_game_switch_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{"irq_depth", 0666, NULL, &proc_get_irq_depth_fops, NULL, false, true},
-	{
-		"oplus_tp_noise_modetest", 0664, NULL, &proc_noise_modetest_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{"tp_fw_update", 0666, NULL, &proc_fw_update_ops, NULL, false, true},
-	{"oplus_register_info", 0664, NULL, &proc_register_info_fops, NULL, false, true},
-	{
-		"incell_panel", 0664, NULL, &proc_incell_panel_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"fd_enable", 0666, NULL, &tp_fd_enable_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"event_num", 0666, NULL, &tp_event_num_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"fd_touch_count", 0666, NULL, &fd_touch_num_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"fp_enable", 0666, NULL, &tp_fp_enable_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"baseline_test", 0666, NULL, &tp_auto_test_proc_fops, NULL, false, true
-	},
-	{
-		"black_screen_test", 0666, NULL, &proc_black_screen_test_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"baseline_result", 0666, NULL, &tp_auto_test_result_fops, NULL, false, true
-	},
-	{
-		"black_screen_result", 0666, NULL, &proc_black_screen_result_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"oplus_tp_direction", 0666, NULL, &touch_dir_proc_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"report_rate_white_list", 0666, NULL, &proc_rate_white_list_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"charge_detect", 0666, NULL, &proc_switch_usb_state_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"wireless_charge_detect", 0666, NULL, &proc_wireless_charge_detect_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"headset_detect", 0666, NULL, &proc_headset_detect_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"tp_aging_test", 0666, NULL, &proc_aging_test_ops, NULL, false,
-		true
-	},
-	{
-		"smooth_level", 0666, NULL, &proc_smooth_level_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"sensitive_level", 0666, NULL, &proc_sensitive_level_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"double_tap_enable_indep", 0666, NULL, &proc_gesture_control_indep_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"calibration", 0666, NULL, &proc_calibrate_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	{
-		"calibration_status", 0666, NULL, &proc_cal_status_fops, NULL, false,
-		false  /* Will be updated later */
-	},
-	/* proc/touchpanel/oplus_apk. Add the new test node for debug and apk. By zhangping 20190402 start*/
-#ifdef CONFIG_OPLUS_TP_APK
-	{"oplus_apk", 0666, NULL, &proc_oplus_apk_fops, NULL, false, true},
-#endif /* end of CONFIG_OPLUS_TP_APK*/
-};
-
 int init_touchpanel_proc(struct touchpanel_data *ts)
 {
 	int ret = 0;
-	int i = 0;
 	struct proc_dir_entry *prEntry_tp = NULL;
 	char name[TP_NAME_SIZE_MAX];
-	
-	/* Create a local copy of the template and update fields that depend on ts */
-	tp_proc_node tp_proc_nodes[ARRAY_SIZE(tp_proc_nodes_template)];
-	
-	/* Copy the template */
-	for (i = 0; i < ARRAY_SIZE(tp_proc_nodes_template); i++) {
-		tp_proc_nodes[i] = tp_proc_nodes_template[i];
-		/* Update fields that depend on ts */
-		if (tp_proc_nodes[i].data == NULL && (
-			strcmp(tp_proc_nodes[i].name, "debug_level") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "double_tap_enable") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "coordinate") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "game_switch_enable") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "irq_depth") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "oplus_tp_noise_modetest") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "tp_fw_update") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "oplus_register_info") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "incell_panel") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "fd_enable") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "event_num") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "fd_touch_count") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "fp_enable") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "baseline_test") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "black_screen_test") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "baseline_result") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "black_screen_result") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "oplus_tp_direction") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "report_rate_white_list") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "charge_detect") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "wireless_charge_detect") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "headset_detect") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "tp_aging_test") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "smooth_level") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "sensitive_level") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "double_tap_enable_indep") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "calibration") == 0 ||
-			strcmp(tp_proc_nodes[i].name, "calibration_status") == 0
-#ifdef CONFIG_OPLUS_TP_APK
-			|| strcmp(tp_proc_nodes[i].name, "oplus_apk") == 0
-#endif
-			)) {
-			tp_proc_nodes[i].data = ts;
-		}
-	}
-	
-	/* Update the is_support field based on ts values */
-	for (i = 0; i < ARRAY_SIZE(tp_proc_nodes); i++) {
-		if (strcmp(tp_proc_nodes[i].name, "double_tap_enable") == 0) {
-			tp_proc_nodes[i].is_support = ts->black_gesture_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "coordinate") == 0) {
-			tp_proc_nodes[i].is_support = ts->black_gesture_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "game_switch_enable") == 0) {
-			tp_proc_nodes[i].is_support = ts->game_switch_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "oplus_tp_noise_modetest") == 0) {
-			tp_proc_nodes[i].is_support = ts->noise_modetest_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "incell_panel") == 0) {
-			tp_proc_nodes[i].is_support = ts->is_incell_panel;
-		} else if (strcmp(tp_proc_nodes[i].name, "fd_enable") == 0) {
-			tp_proc_nodes[i].is_support = ts->face_detect_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "event_num") == 0) {
-			tp_proc_nodes[i].is_support = ts->face_detect_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "fd_touch_count") == 0) {
-			tp_proc_nodes[i].is_support = ts->face_detect_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "fp_enable") == 0) {
-			tp_proc_nodes[i].is_support = ts->fingerprint_underscreen_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "black_screen_test") == 0) {
-			tp_proc_nodes[i].is_support = ts->gesture_test_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "black_screen_result") == 0) {
-			tp_proc_nodes[i].is_support = ts->gesture_test_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "oplus_tp_direction") == 0) {
-			tp_proc_nodes[i].is_support = ts->fw_edge_limit_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "report_rate_white_list") == 0) {
-			tp_proc_nodes[i].is_support = ts->report_rate_white_list_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "charge_detect") == 0) {
-			tp_proc_nodes[i].is_support = ts->charger_pump_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "wireless_charge_detect") == 0) {
-			tp_proc_nodes[i].is_support = ts->wireless_charger_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "headset_detect") == 0) {
-			tp_proc_nodes[i].is_support = ts->headset_pump_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "smooth_level") == 0) {
-			tp_proc_nodes[i].is_support = ts->smooth_level_array_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "sensitive_level") == 0) {
-			tp_proc_nodes[i].is_support = ts->sensitive_level_array_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "double_tap_enable_indep") == 0) {
-			tp_proc_nodes[i].is_support = ts->black_gesture_indep_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "calibration") == 0) {
-			tp_proc_nodes[i].is_support = ts->auto_test_need_cal_support;
-		} else if (strcmp(tp_proc_nodes[i].name, "calibration_status") == 0) {
-			tp_proc_nodes[i].is_support = ts->auto_test_need_cal_support;
-		}
-	}
 
 	TP_INFO(ts->tp_index, "%s entry\n", __func__);
 
@@ -3302,22 +3099,276 @@ int init_touchpanel_proc(struct touchpanel_data *ts)
 
 	ts->prEntry_tp = prEntry_tp;
 
-	for (i = 0; i < ARRAY_SIZE(tp_proc_nodes); i++) {
-		if (tp_proc_nodes[i].is_support) {
-			tp_proc_nodes[i].node = proc_create_data(tp_proc_nodes[i].name,
-								tp_proc_nodes[i].mode,
-								prEntry_tp, tp_proc_nodes[i].fops, tp_proc_nodes[i].data);
-
-			if (tp_proc_nodes[i].node == NULL) {
-				tp_proc_nodes[i].is_created = false;
-				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/%s\n", __func__,
-					tp_proc_nodes[i].name);
+	/* Process each proc node individually to avoid stack allocation */
+	{
+		/* oplus_optimized_time */
+		if (false) { /* This is always false, no need to create */ }
+		
+		/* tp_index */
+		if (false) { /* This is always false, no need to create */ }
+		
+		/* debug_level */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("debug_level", 0644, prEntry_tp, &proc_debug_level_ops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/debug_level\n", __func__);
 				ret = -ENODEV;
-
-			} else {
-				tp_proc_nodes[i].is_created = true;
 			}
 		}
+		
+		/* double_tap_enable */
+		if (ts->black_gesture_support) {
+			struct proc_dir_entry *node = proc_create_data("double_tap_enable", 0666, prEntry_tp, &proc_gesture_control_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/double_tap_enable\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* coordinate */
+		if (ts->black_gesture_support) {
+			struct proc_dir_entry *node = proc_create_data("coordinate", 0444, prEntry_tp, &proc_coordinate_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/coordinate\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* game_switch_enable */
+		if (ts->game_switch_support) {
+			struct proc_dir_entry *node = proc_create_data("game_switch_enable", 0666, prEntry_tp, &proc_game_switch_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/game_switch_enable\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* irq_depth */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("irq_depth", 0666, prEntry_tp, &proc_get_irq_depth_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/irq_depth\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* oplus_tp_noise_modetest */
+		if (ts->noise_modetest_support) {
+			struct proc_dir_entry *node = proc_create_data("oplus_tp_noise_modetest", 0664, prEntry_tp, &proc_noise_modetest_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/oplus_tp_noise_modetest\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* tp_fw_update */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("tp_fw_update", 0666, prEntry_tp, &proc_fw_update_ops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/tp_fw_update\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* oplus_register_info */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("oplus_register_info", 0664, prEntry_tp, &proc_register_info_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/oplus_register_info\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* incell_panel */
+		if (ts->is_incell_panel) {
+			struct proc_dir_entry *node = proc_create_data("incell_panel", 0664, prEntry_tp, &proc_incell_panel_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/incell_panel\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* fd_enable */
+		if (ts->face_detect_support) {
+			struct proc_dir_entry *node = proc_create_data("fd_enable", 0666, prEntry_tp, &tp_fd_enable_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/fd_enable\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* event_num */
+		if (ts->face_detect_support) {
+			struct proc_dir_entry *node = proc_create_data("event_num", 0666, prEntry_tp, &tp_event_num_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/event_num\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* fd_touch_count */
+		if (ts->face_detect_support) {
+			struct proc_dir_entry *node = proc_create_data("fd_touch_count", 0666, prEntry_tp, &fd_touch_num_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/fd_touch_count\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* fp_enable */
+		if (ts->fingerprint_underscreen_support) {
+			struct proc_dir_entry *node = proc_create_data("fp_enable", 0666, prEntry_tp, &tp_fp_enable_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/fp_enable\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* baseline_test */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("baseline_test", 0666, prEntry_tp, &tp_auto_test_proc_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/baseline_test\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* black_screen_test */
+		if (ts->gesture_test_support) {
+			struct proc_dir_entry *node = proc_create_data("black_screen_test", 0666, prEntry_tp, &proc_black_screen_test_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/black_screen_test\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* baseline_result */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("baseline_result", 0666, prEntry_tp, &tp_auto_test_result_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/baseline_result\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* black_screen_result */
+		if (ts->gesture_test_support) {
+			struct proc_dir_entry *node = proc_create_data("black_screen_result", 0666, prEntry_tp, &proc_black_screen_result_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/black_screen_result\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* oplus_tp_direction */
+		if (ts->fw_edge_limit_support) {
+			struct proc_dir_entry *node = proc_create_data("oplus_tp_direction", 0666, prEntry_tp, &touch_dir_proc_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/oplus_tp_direction\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* report_rate_white_list */
+		if (ts->report_rate_white_list_support) {
+			struct proc_dir_entry *node = proc_create_data("report_rate_white_list", 0666, prEntry_tp, &proc_rate_white_list_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/report_rate_white_list\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* charge_detect */
+		if (ts->charger_pump_support) {
+			struct proc_dir_entry *node = proc_create_data("charge_detect", 0666, prEntry_tp, &proc_switch_usb_state_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/charge_detect\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* wireless_charge_detect */
+		if (ts->wireless_charger_support) {
+			struct proc_dir_entry *node = proc_create_data("wireless_charge_detect", 0666, prEntry_tp, &proc_wireless_charge_detect_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/wireless_charge_detect\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* headset_detect */
+		if (ts->headset_pump_support) {
+			struct proc_dir_entry *node = proc_create_data("headset_detect", 0666, prEntry_tp, &proc_headset_detect_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/headset_detect\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* tp_aging_test */
+		if (true) { /* always supported */
+			struct proc_dir_entry *node = proc_create_data("tp_aging_test", 0666, prEntry_tp, &proc_aging_test_ops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/tp_aging_test\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* smooth_level */
+		if (ts->smooth_level_array_support) {
+			struct proc_dir_entry *node = proc_create_data("smooth_level", 0666, prEntry_tp, &proc_smooth_level_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/smooth_level\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* sensitive_level */
+		if (ts->sensitive_level_array_support) {
+			struct proc_dir_entry *node = proc_create_data("sensitive_level", 0666, prEntry_tp, &proc_sensitive_level_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/sensitive_level\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* double_tap_enable_indep */
+		if (ts->black_gesture_indep_support) {
+			struct proc_dir_entry *node = proc_create_data("double_tap_enable_indep", 0666, prEntry_tp, &proc_gesture_control_indep_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/double_tap_enable_indep\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* calibration */
+		if (ts->auto_test_need_cal_support) {
+			struct proc_dir_entry *node = proc_create_data("calibration", 0666, prEntry_tp, &proc_calibrate_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/calibration\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+		/* calibration_status */
+		if (ts->auto_test_need_cal_support) {
+			struct proc_dir_entry *node = proc_create_data("calibration_status", 0666, prEntry_tp, &proc_cal_status_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/calibration_status\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+		
+#ifdef CONFIG_OPLUS_TP_APK
+		/* oplus_apk */
+		if (true) { /* always supported when enabled */
+			struct proc_dir_entry *node = proc_create_data("oplus_apk", 0666, prEntry_tp, &proc_oplus_apk_fops, ts);
+			if (node == NULL) {
+				TP_INFO(ts->tp_index, "%s: Couldn't create proc/debug_info/oplus_apk\n", __func__);
+				ret = -ENODEV;
+			}
+		}
+#endif
 	}
 
 	/*create debug_info node*/
